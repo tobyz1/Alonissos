82b6a8263822ae9a2820435f9c2f0fd280aec10b
Baptiste Dupre <baptiste.dupre.59pgmail.com>
2023-07-19 19:30:48 +0200

    changement de design du header mais celui final sans la partie responsive

--- script.js
+++ script.js
@@ -7,12 +7,4 @@ hamburgerButton.addEventListener("click", toggleNav);
 function toggleNav() {
   hamburgerButton.classList.toggle("active");
   navigation.classList.toggle("active");
-
-  // Récupérer tous les éléments avec la classe "link"
-  var elements = document.getElementsByClassName('link');
-
-  // Parcourir tous les éléments et leur ajouter la classe "active"
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].classList.toggle("active");
-  }
 }
