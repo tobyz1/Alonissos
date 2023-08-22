1559aab1faad4727be9ca44972e115097127b0da
Baptiste Dupre <baptiste.dupre.59@gmail.com>
2023-08-22 15:57:56 +0300

    fin caroussel

--- script/script.js
+++ script/script.js
@@ -10,3 +10,34 @@ function toggleNav() {
 }
 
 
+const slider = document.querySelector(".slider");
+
+const leftArrow = document.querySelector(".left");
+const rightArrow = document.querySelector(".right");
+const indicatorParents = document.querySelector('.slider-controls ul');
+let sectionIndex = 0;
+let numberOfSlide = 10;
+
+
+document.querySelectorAll('.slider-controls li').forEach(function (indicator, ind) {
+    indicator.addEventListener('click', function () {
+        sectionIndex = ind;
+        document.querySelector('.slider-controls .selected').classList.remove('selected');
+        indicator.classList.add('selected');
+        slider.style.transform = 'translate(' + (sectionIndex) * -100/numberOfSlide +'%)';
+    });
+});
+
+rightArrow.addEventListener('click', function () {
+    sectionIndex = (sectionIndex < numberOfSlide-1) ? sectionIndex + 1 : numberOfSlide-1;
+    document.querySelector('.slider-controls .selected').classList.remove('selected');
+    indicatorParents.children[sectionIndex].classList.add('selected');
+    slider.style.transform = 'translate(' + (sectionIndex) * -100/numberOfSlide +'%)';
+});
+
+leftArrow.addEventListener('click', function () {
+    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+    document.querySelector('.slider-controls .selected').classList.remove('selected');
+    indicatorParents.children[sectionIndex].classList.add('selected');
+    slider.style.transform = 'translate(' + (sectionIndex) * -100/numberOfSlide +'%)';
+});
