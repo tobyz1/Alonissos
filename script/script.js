510732ede10ca41419d9f3c16001e24823b909dd
Baptiste Dupre <baptiste.dupre.59@gmail.com>
2023-08-26 16:52:36 +0300

    oooof ça avance

--- script/script.js
+++ script/script.js
@@ -48,8 +48,14 @@ let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('e
 
 const calendar = document.getElementById('calendar')
 const newEventModal = document.getElementById('newEventModal')
+const deleteEventModal = document.getElementById('deleteEventModal')
 const backDrop = document.getElementById('modalBackDrop')
+const eventTitleInput = document.getElementById('eventTitleInput')
 const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+const nameOfBooker = document.getElementById('name')
+const emailOfBooker = document.getElementById('email')
+const dateReservation = document.getElementsByClassName('dateReservation')
+
 
 function openModal(date) {
     clicked = date
@@ -57,11 +63,14 @@ function openModal(date) {
     const eventForDay = events.find(e => e.date == clicked)
 
     if (eventForDay) {
-        console.log('Event already exists')
+        document.getElementById('eventText').innerText = eventForDay.title
+        deleteEventModal.style.display = 'block'
     } else {
         newEventModal.style.display = 'block'
     }
+    backDrop.style.display = 'flex'
 }
+
 
 function load() {
     const dt = new Date()
@@ -92,11 +101,23 @@ function load() {
     for (let i = 1; i <= paddingDays + daysInMonth; i++) {
         const daySquare = document.createElement('div')
         daySquare.classList.add('day')
-
+        const dayString = `${i - paddingDays}/${month + 1}/${year}`
         if (i > paddingDays) {
             daySquare.innerText = i - paddingDays
+            const eventForDay = events.find(e => e.date === dayString)
+            if (i - paddingDays === day && nav === 0) {
+                daySquare.id = 'currentDay'
+            }
 
-            daySquare.addEventListener('click', () => console.log('click'))
+            if (eventForDay) {
+                const eventDiv = document.createElement('div')
+                eventDiv.classList.add('event')
+                eventDiv.innerText = eventForDay.title
+                daySquare.classList.replace('day', 'bookDay')
+                daySquare.appendChild(eventDiv)        
+            }
+
+            daySquare.addEventListener('click', () => openModal(dayString))
         } else {
             daySquare.classList.add('padding')
         }
@@ -104,6 +125,40 @@ function load() {
     }
 }
 
+
+function closeModal() {
+    eventTitleInput.classList.remove('error')
+    newEventModal.style.display = 'none'
+    deleteEventModal.style.display = 'none'
+    backDrop.style.display = 'none'
+    eventTitleInput.value = ''
+    nameOfBooker.value = ''
+    emailOfBooker.value = ''
+    dateReservation.value = ''
+    clicked = null
+    load()
+}
+
+function saveEvent() {
+    if (eventTitleInput.value) {
+        eventTitleInput.classList.remove('error')
+
+        events.push({
+            date: clicked,
+            title: eventTitleInput.value
+        })
+
+        localStorage.setItem('events', JSON.stringify(events))
+        closeModal()
+    } else {
+        eventTitleInput.classList.add('error')
+    }
+} 
+function deleteEvent() {
+    events = events.filter(e => e.date !== clicked)
+    localStorage.setItem('events', JSON.stringify(events))
+    closeModal()
+}
 function initButtons() {
     document.getElementById('nextButton').addEventListener('click', () => {
         nav++
@@ -113,10 +168,13 @@ function initButtons() {
         nav--
         load()
     })
+    document.getElementById('saveButton').addEventListener('click', saveEvent)
+    document.getElementById('cancelButton').addEventListener('click', closeModal)
+
+    document.getElementById('deleteButton').addEventListener('click', deleteEvent)
+    document.getElementById('closeButton').addEventListener('click', closeModal)
 }
-
-
-
 
 initButtons()
 load()
+
